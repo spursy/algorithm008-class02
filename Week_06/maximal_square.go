d3cd31b12fafb9d5386020af91179fa9ccf9e97f
yang yang <yang.yang@5th.work>
2020-05-31 23:39:47 +0800

    update code

--- Week_06/maximal_square.go
+++ Week_06/maximal_square.go
@@ -1,27 +1,22 @@
-package main
+package main;
 import "fmt"
 
 func maximalSquare(matrix [][]byte) int {
   dp := make([][]int, len(matrix))
   maxSide := 0
-  for i := 0; i < len(matrix); i++ {
-      dp[i] = make([]int, len(matrix[i]))
-      for j := 0; j < len(matrix[i]); j++ {
-          dp[i][j] = int(matrix[i][j] - '0')
-          if dp[i][j] == 1 {
-              maxSide = 1
-          }
-      }
-  }
 
-  for i := 1; i < len(matrix); i++ {
-      for j := 1; j < len(matrix[i]); j++ {
-          if dp[i][j] == 1 {
-              dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+  for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+      for j := 0; j < len(matrix[i]); j++ {
+					if i == 0 || j == 0 {
+						dp[i][j] =  int(matrix[i][j] - '0')
+					} else if int(matrix[i][j] - '0') == 1 {
+							dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
               if dp[i][j] > maxSide {
                   maxSide = dp[i][j]
               }
-          }
+					}
+					
       }
   }
   return maxSide * maxSide
@@ -34,3 +29,13 @@ func min(x, y int) int {
   return y
 }
 
+func main() {
+	var origin = [][] byte {
+		{ '1', '0', '1', '0', '0' },
+		{ '1', '0', '1', '1', '1' },
+		{ '1', '1', '1', '1', '1' },
+		{ '1', '0', '0', '1', '0' },
+	}
+	var result = maximalSquare(origin)
+	fmt.Println(result)
+}
